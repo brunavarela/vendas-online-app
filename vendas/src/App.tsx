59466b8a1fbf43f69f478235d71d350947e89b14
Bruna Varela <bruna@parkaware.com.br>
2024-01-30 12:34:21 -0300

    chore: install dependences

---
+++ vendas/src/App.tsx
@@ -0,0 +1,11 @@
+import {SafeAreaView, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Testanto</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
