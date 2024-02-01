f7bd46c588c0170e2c722d68fe00963244a168c6
Bruna Varela <bruna@parkaware.com.br>
2024-02-01 12:43:33 -0300

    feat: create modules login

--- vendas/src/App.tsx
+++ vendas/src/App.tsx
@@ -1,11 +1,11 @@
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import Login from './modules/login';
 import styled from 'styled-components/native';
-
 
 const App = () => {
   return (
     <SafeAreaView>
-      <Text>Testando</Text>
+      <Login />
     </SafeAreaView>
   );
 };
