f59e6804c6fe10a3114f3730d1b9cc75df3a2ad1
Bruna Varela <brunavarela1@hotmail.com>
2024-04-17 15:32:36 -0300

    feat: insert types in Text

--- vendas/src/shared/components/button/button.tsx
+++ vendas/src/shared/components/button/button.tsx
@@ -1,20 +1,20 @@
 import {TouchableOpacityProps} from 'react-native';
 import {ButtonContainer} from './button.style';
 import Text from '../text/Text';
-import { theme } from '../../themes/theme';
-import { textTypes } from '../text/textTypes';
+import {theme} from '../../themes/theme';
+import {textTypes} from '../text/textTypes';
 
 //Mesma coisa do input, ja tenho todos os tipos
 //padrão do component touchableOpacity, incluindo o onPress
 interface ButtonProps extends TouchableOpacityProps {
   title: string;
-  margin?: string
+  margin?: string;
 }
 
 const Button = ({title, margin, ...props}: ButtonProps) => {
   return (
     <ButtonContainer margin={margin} {...props}>
-      <Text color={theme.colors.neutralTheme.white}>{title}</Text>
+      <Text type={textTypes.BUTTON_BOLD} color={theme.colors.neutralTheme.white}>{title}</Text>
     </ButtonContainer>
   );
 };
--- vendas/src/shared/components/text/text.style.ts
+++ vendas/src/shared/components/text/text.style.ts
@@ -3,10 +3,12 @@ import styled from 'styled-components/native';
 interface ContainerTextProps {
   color?: string;
   fontSize: string;
+  fontFamily: 'Poppins-Bold' | 'Poppins-Light' | 'Poppins-Regular';
 }
 
 export const ContainerText = styled.Text<ContainerTextProps>`
   ${(props: {color: any}) => (props.color ? `color: ${props.color}` : '')};
 
   font-size: ${(props: {fontSize: any}) => props.fontSize};
+  font-family: ${(props: {fontFamily: any}) => props.fontFamily};
 `;
