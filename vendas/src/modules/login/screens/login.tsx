121a2d9303fe396cbdade035cbec5bdda18b82b5
Bruna Varela <brunavarela1@hotmail.com>
2024-09-30 21:13:22 -0300

    feat: insert icons

--- vendas/src/modules/login/screens/login.tsx
+++ vendas/src/modules/login/screens/login.tsx
@@ -3,6 +3,7 @@ import { ContainerLogin } from '../styles/login.style';
 import Input from '../../../shared/components/input/Input';
 import Button from '../../../shared/components/button/Button';
 import { theme } from '../../../shared/themes/theme';
+import { Icon } from '../../../shared/components/icon/Icon';
 <script src="http://localhost:8097"></script>
 
 const Login = () => {
@@ -13,6 +14,7 @@ const Login = () => {
   return (
     <View>
       <ContainerLogin>
+        <Icon name="home3" color="blue" size={44}/>
         <Input errorMessage='Usuário ou senha inválidos' placeholder='Digite seu email' title='Email:'/>
         <Button 
           type={theme.buttons.buttonsTheme.primary} 
