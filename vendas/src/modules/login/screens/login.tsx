405d8fdb07d011d6253a734054f3671492bbb640
Bruna Varela <brunavarela1@hotmail.com>
2025-04-29 18:36:16 -0300

    feat: validate email and password in login screen

--- vendas/src/modules/login/screens/login.tsx
+++ vendas/src/modules/login/screens/login.tsx
@@ -3,21 +3,43 @@ import { ContainerLogin, ImageLogo } from '../styles/login.style';
 import Input from '../../../shared/components/input/Input';
 import Button from '../../../shared/components/button/Button';
 import { theme } from '../../../shared/themes/theme';
-<script src="http://localhost:8097"></script>
+import { useLogin } from '../hooks/useLogin';
 
 const Login = () => {
-  const handleOnPress = () => {
-    console.log('clicou');
-  };
+  const {
+    email,
+    password,
+    loading,
+    errorMessage,
+    handleOnPress,
+    handleOnChangeEmail,
+    handleOnChangePassword
+  } = useLogin();
 
   return (
     <View>
       <ContainerLogin>
-        <ImageLogo resizeMode="contain" source={require('../../../assets/images/Abba.png')} />
-        <Input margin='0px 0px 8px 0px' placeholder='Digite seu email' title='Email:'/>
-        <Input secureTextEntry placeholder='Digite sua senha' title='Senha:'/>
+        <ImageLogo 
+          resizeMode="contain" 
+          source={require('../../../assets/images/Abba.png')} />
+        <Input 
+          value={email}
+          errorMessage={errorMessage} 
+          margin='0px 0px 8px 0px' 
+          placeholder='Digite seu email' 
+          title='Email:'
+          onChange={handleOnChangeEmail}
+        />
+        <Input 
+          value={password}
+          secureTextEntry 
+          placeholder='Digite sua senha' 
+          title='Senha:'
+          onChange={handleOnChangePassword}
+        />
         <Button 
           type={theme.buttons.buttonsTheme.primary} 
+          loading={loading}
           margin="16px" 
           title="ENTRAR" 
           onPress={handleOnPress}
