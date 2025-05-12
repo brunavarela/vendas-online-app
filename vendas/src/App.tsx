c5d25d52943e9c4d262738d66d781da860ff35e9
Bruna Varela <brunavarela1@hotmail.com>
2025-05-12 17:40:41 -0300

    fix: react-native-screens and react-native-safe-area-context versions

--- vendas/src/App.tsx
+++ vendas/src/App.tsx
@@ -3,13 +3,25 @@ import {SafeAreaView} from 'react-native';
 import store from './store'
 import { Provider } from 'react-redux'
 import GlobalModal from './shared/components/modal/globalModal/GlobalModal';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <Provider store={store}>
       <GlobalModal/>
       <SafeAreaView>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Login"
+              component={Login}
+            />
+            <Stack.Screen name="Home" component={Login} options={{title: 'Home'}}/>
+          </Stack.Navigator>
+        </NavigationContainer>
         <Login />
       </SafeAreaView>
     </Provider>
