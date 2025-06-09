9e1f80de165ed93bde57f1e5afac4daf354d5c2f
Bruna Varela <brunavarela1@hotmail.com>
2025-06-09 20:39:28 -0300

    feat: insert ref and keyboardtype in input

--- vendas/src/modules/createUser/screens/createUser.tsx
+++ vendas/src/modules/createUser/screens/createUser.tsx
@@ -1,68 +1,91 @@
-import Input from "../../../shared/components/input/Input";
-import Button from "../../../shared/components/button/Button";
-import { CreateUserContainer } from "../styles/createUser.style";
-import { useCreateUser } from "../hooks/useCreateUser";
+import { useRef } from 'react';
+import { TextInput } from 'react-native/types';
+
+import Button from '../../../shared/components/button/Button';
+import Input from '../../../shared/components/input/Input';
+import { useCreateUser } from '../hooks/useCreateUser';
+import { CreateUserContainer } from '../styles/createUser.style';
 
 const CreateUser = () => {
-    const {createUser, loading, disable, handleOnChangeInput, handleCreateUser} = useCreateUser();
+  const { createUser, disabled, loading, handleOnChangeInput, handleCreateUser } = useCreateUser();
 
-    return (
-        <CreateUserContainer>
-            <Input
-               value={createUser.name} 
-               onChange={(event) => handleOnChangeInput(event, 'name')}
-               margin="0px 0px 16px 0px" 
-               placeholder="Digite" 
-               title="Nome completo:"
-            />
-            <Input
-               value={createUser.phone} 
-               onChange={(event) => handleOnChangeInput(event, 'phone')}
-               margin="0px 0px 16px 0px" 
-               placeholder="Digite" 
-               type="cel-phone"
-               title="Telefone:"
-            />
-            <Input
-               value={createUser.email} 
-               onChange={(event) => handleOnChangeInput(event, 'email')}
-               margin="0px 0px 16px 0px" 
-               placeholder="Digite" 
-               title="Email:"
-            />
-            <Input
-               value={createUser.cpf} 
-               onChange={(event) => handleOnChangeInput(event, 'cpf')}
-               margin="0px 0px 16px 0px" 
-               placeholder="Digite" 
-               type="cpf"
-               title="cpf:"
-            />
-            <Input
-               value={createUser.password} 
-               onChange={(event) => handleOnChangeInput(event, 'password')}
-               margin="0px 0px 16px 0px" 
-               placeholder="Digite" 
-               title="Senha:"
-               secureTextEntry
-            />
-            <Input
-               value={createUser.confirmPassword} 
-               onChange={(event) => handleOnChangeInput(event, 'confirmPassword')}
-               margin="0px 0px 16px 0px" 
-               placeholder="Digite" 
-               title="Confirmar senha:"
-               secureTextEntry
-            /> 
-            <Button 
-               disabled={disable}
-               onPress={handleCreateUser}
-               loading={loading}
-               margin="0px 0px 32px 0px" 
-               title="Criar usuário" 
-            />
-        </CreateUserContainer>
-    )
-}
+  const phoneInput = useRef<TextInput>(null);
+  const emailInput = useRef<TextInput>(null);
+  const cpfInput = useRef<TextInput>(null);
+  const passwordInput = useRef<TextInput>(null);
+  const confirmPasswordInput = useRef<TextInput>(null);
+
+  return (
+    <CreateUserContainer>
+      <Input
+        value={createUser.name}
+        onChange={(event) => handleOnChangeInput(event, 'name')}
+        margin="0px 0px 16px 0px"
+        placeholder="Digite"
+        title="Nome completo:"
+        onSubmitEditing={() => phoneInput?.current?.focus()}
+      />
+      <Input
+        value={createUser.phone}
+        onChange={(event) => handleOnChangeInput(event, 'phone')}
+        margin="0px 0px 16px 0px"
+        placeholder="Digite"
+        type="cel-phone"
+        title="Telefone:"
+        ref={phoneInput}
+        onSubmitEditing={() => emailInput?.current?.focus()}
+        keyboardType="number-pad"
+      />
+      <Input
+        value={createUser.email}
+        onChange={(event) => handleOnChangeInput(event, 'email')}
+        margin="0px 0px 16px 0px"
+        placeholder="Digite"
+        title="Email:"
+        ref={emailInput}
+        onSubmitEditing={() => cpfInput?.current?.focus()}
+        keyboardType="email-address"
+      />
+      <Input
+        value={createUser.cpf}
+        onChange={(event) => handleOnChangeInput(event, 'cpf')}
+        margin="0px 0px 16px 0px"
+        placeholder="Digite"
+        type="cpf"
+        title="CPF:"
+        ref={cpfInput}
+        onSubmitEditing={() => passwordInput?.current?.focus()}
+        keyboardType="number-pad"
+      />
+      <Input
+        value={createUser.password}
+        onChange={(event) => handleOnChangeInput(event, 'password')}
+        margin="0px 0px 16px 0px"
+        placeholder="Digite"
+        title="Senha:"
+        secureTextEntry
+        ref={passwordInput}
+        onSubmitEditing={() => confirmPasswordInput?.current?.focus()}
+      />
+      <Input
+        value={createUser.confirmPassword}
+        onChange={(event) => handleOnChangeInput(event, 'confirmPassword')}
+        margin="0px 0px 16px 0px"
+        placeholder="Digite"
+        title="Confirmar senha:"
+        secureTextEntry
+        ref={confirmPasswordInput}
+        onSubmitEditing={handleCreateUser}
+      />
+      <Button
+        disabled={disabled}
+        onPress={handleCreateUser}
+        loading={loading}
+        margin="0px 0px 32px 0px"
+        title="Criar usuário"
+      />
+    </CreateUserContainer>
+  );
+};
 
 export default CreateUser;
--- vendas/src/shared/components/input/input.tsx
+++ vendas/src/shared/components/input/input.tsx
@@ -1,10 +1,16 @@
-import { NativeSyntheticEvent, TextInputChangeEventData, TextInputProps, View } from 'react-native';
+import React, { useState, forwardRef } from 'react';
+import {
+  NativeSyntheticEvent,
+  TextInputChangeEventData,
+  TextInputProps,
+  View,
+  TextInput
+} from 'react-native';
 import { ContainerInput, IconEye } from './input.style';
 import { DisplayFlexColumn } from '../globalStyles/globalView.style';
 import Text from '../text/Text';
 import { textTypes } from '../text/textTypes';
 import { theme } from '../../themes/theme';
-import { useState } from 'react';
 import { insertMaskInCpf } from '../../functions/cpf';
 import { insertMaskInPhone } from '../../functions/phone';
 
@@ -16,19 +22,19 @@ interface InputProps extends TextInputProps {
   type: 'cel-phone' | 'cpf';
 }
 
-const Input = ({ 
-    margin, 
-    secureTextEntry, 
-    title, 
-    errorMessage, 
-    onChange, 
-    type,
-    ...props 
-  }: InputProps) => {
+const Input = forwardRef<TextInput, InputProps>(({
+  margin,
+  secureTextEntry,
+  title,
+  errorMessage,
+  onChange,
+  type,
+  ...props
+}, ref) => {
   const [currentSecure, setCurrentSecure] = useState<boolean>(!!secureTextEntry);
 
   const handleOnChange = (event: NativeSyntheticEvent<TextInputChangeEventData>) => {
-    if(onChange) {
+    if (onChange) {
       let text = event.nativeEvent.text;
       switch (type) {
         case 'cpf':
@@ -37,11 +43,9 @@ const Input = ({
         case 'cel-phone':
           text = insertMaskInPhone(text);
           break;
-    
         default:
-          text = event.nativeEvent.text;
           break;
-      };
+      }
 
       onChange({
         ...event,
@@ -50,48 +54,52 @@ const Input = ({
           text,
         }
       });
-    };
-  } 
-  
+    }
+  };
+
   const handleOnPressEye = () => {
-    setCurrentSecure((current) => !current)
-  }
+    setCurrentSecure((current) => !current);
+  };
 
   return (
     <DisplayFlexColumn customMargin={margin}>
       {title && (
-        <Text 
-          margin='0px 0px 4px 8px' 
-          color={theme.colors.grayTheme.gray100} 
-          type={textTypes.PARAGRAPH_SMALL_SEMIBOLD}>
-            {title}
+        <Text
+          margin='0px 0px 4px 8px'
+          color={theme.colors.grayTheme.gray100}
+          type={textTypes.PARAGRAPH_SMALL_SEMIBOLD}
+        >
+          {title}
         </Text>
       )}
       <View>
-        <ContainerInput 
+        <ContainerInput
           {...props}
-          hasSecureTextEntry={secureTextEntry} 
-          secureTextEntry={currentSecure} 
+          ref={ref}
+          hasSecureTextEntry={secureTextEntry}
+          secureTextEntry={currentSecure}
           isError={!!errorMessage}
           onChange={handleOnChange}
         />
         {secureTextEntry && (
-          <IconEye 
-            onPress={handleOnPressEye} 
-            name={currentSecure ? "eye-blocked" : "eye"} 
+          <IconEye
+            onPress={handleOnPressEye}
+            name={currentSecure ? "eye-blocked" : "eye"}
             size={20}
-          />)}
+          />
+        )}
       </View>
       {errorMessage && (
-        <Text 
-          margin='4px 0px 0px 8px' 
-          color={theme.colors.orangeTheme.orange80} 
-          type={textTypes.PARAGRAPH_SMALL_SEMIBOLD}>
-            {errorMessage}
+        <Text
+          margin='4px 0px 0px 8px'
+          color={theme.colors.orangeTheme.orange80}
+          type={textTypes.PARAGRAPH_SMALL_SEMIBOLD}
+        >
+          {errorMessage}
         </Text>
       )}
     </DisplayFlexColumn>
-  )
-};
+  );
+});
 
 export default Input;
