b0b8bd50abc51c3eb8c94730d70ae3025a62fffd
Bruna Varela <brunavarela1@hotmail.com>
2025-06-09 19:25:18 -0300

    feat: insert validate cpf, email and phone

--- vendas/src/modules/createUser/hooks/useCreateUser.ts
+++ vendas/src/modules/createUser/hooks/useCreateUser.ts
@@ -6,6 +6,9 @@ import { MethodEnum } from "../../../enums/methods.enum";
 import { URL_USER } from "../../../shared/constants/urls";
 import { NavigationProp, ParamListBase, useNavigation } from "@react-navigation/native";
 import { MenuUrl } from "../../../shared/enums/MenuUrl.enum";
+import { validateCpf } from "../../../shared/functions/cpf";
+import { validatePhone } from "../../../shared/functions/phone";
+import { validateEmail } from "../../../shared/functions/email";
 
 export const useCreateUser = () => {
     const { reset } = useNavigation<NavigationProp<ParamListBase>>();
@@ -23,9 +26,9 @@ export const useCreateUser = () => {
     useEffect(() => {
         if (
             createUser.name !== '' &&
-            createUser.phone !== '' &&
-            createUser.email !== '' &&
-            createUser.cpf !== '' &&
+            validatePhone(createUser.phone) &&
+            validateEmail(createUser.email) &&
+            validateCpf(createUser.cpf) &&
             createUser.password !== '' &&
             createUser.password === createUser.confirmPassword
         ) {
